ddb3b199f884117d56a2f6b903b13ff12cb3d791
Malinin Mikhail <infectedfate@gmail.com>
2019-05-19 23:51:42 +0300

    add associate to images model in user and item

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   has_and_belongs_to_many :carts
   has_many :positions
   has_many :carts, through :positions
+  has_many :images, as: :imageable
   validates :price, numericality: {greater_than: 0}
   validates :weight, numericality: {greater_than: 0}, on: :create
   validates :name, presence: true, on: :create

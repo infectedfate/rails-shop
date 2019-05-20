5ce005ee357a818afa3c14d3d9f645a80ff2cb81
Malinin Mikhail <infectedfate@gmail.com>
2019-05-20 17:09:49 +0300

    fix

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,22 +1,17 @@
 class Item < ApplicationRecord
   has_and_belongs_to_many :carts
   has_many :positions
-  has_many :carts, through :positions
+  has_many :carts, through: :positions
   has_many :images, as: :imageable
   validates :price, numericality: {greater_than: 0}
   validates :weight, numericality: {greater_than: 0}, on: :create
   validates :name, presence: true, on: :create
 
-  afted_create :increment_category_counter
-  before_destroy :decrement_category_counter
-
-  private
-
-  def increment_category_counter
+  after_create do
     category.inc!(items_count)
   end
 
-  def decrement_category_counter
+  before_destroy do
     category.inc!(items_count, -1)
-  end
+end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,11 @@
 class Order < ApplicationRecord
   belongs_to :user
+
+  after_create :send_confirmation_email, if: -> { self.user.send_confirmations }
+
+  private
+
+  def send_confirmation_email
+    OrderMail.confirmation(user.mail, order).deliver
+  end
 end

095906134951093138bc29bf548c47c7b3e99f7b
Malinin Mikhail <infectedfate@gmail.com>
2019-05-20 20:19:31 +0300

    add used column to items

--- db/migrate/20190520171754_add_user_column_to_items.rb
+++ db/migrate/20190520171754_add_user_column_to_items.rb
@@ -1,4 +1,5 @@
 class AddUserColumnToItems < ActiveRecord::Migration[5.2]
   def change
+    add_column :items, :used, :boolean, default: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_155846) do
+ActiveRecord::Schema.define(version: 2019_05_20_171754) do
 
   create_table "carts", force: :cascade do |t|
     t.integer "user_id"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2019_05_20_155846) do
     t.integer "price"
     t.integer "weight"
     t.integer "category_id"
+    t.boolean "used", default: false
   end
 
   create_table "orders", force: :cascade do |t|

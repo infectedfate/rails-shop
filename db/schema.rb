d0157bb9c3d65895bfa1cb143e5353f4a37ecd28
Malinin Mikhail <infectedfate@gmail.com>
2019-05-20 18:59:39 +0300

    add name to categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_142243) do
+ActiveRecord::Schema.define(version: 2019_05_20_155846) do
 
   create_table "carts", force: :cascade do |t|
     t.integer "user_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_05_20_142243) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "items_count"
+    t.string "name"
   end
 
   create_table "feedbacks", force: :cascade do |t|
